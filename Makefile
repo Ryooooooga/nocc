97db9da822185f65d44afaef283f4fdff4ebb685
Ryooooooga <ryoga_314@yahoo.co.jp>
2019-04-04 14:07:28 +0900

    整数1つのコンパイラを作成

--- Makefile
+++ Makefile
@@ -1,8 +1,17 @@
 CC      ?= clang
+CXX     ?= clang++
 CFLAGS  ?= -std=c89 -Wall -Wextra -pedantic -Werror
 LDFLAGS ?=
+
+CFLAGS  += $(addprefix -I,$(shell llvm-config --includedir))
+LDFLAGS += $(shell llvm-config --ldflags --system-libs --libs core support analysis)
+
+.PHONY: all clean
 
 all: nocc
 
 nocc: main.o
-	${CC} ${CFLAGS} -o $@ $^ ${LDFLAGS}
+	${CXX} ${CFLAGS} -o $@ $^ ${LDFLAGS}
+
+clean:
+	${RM} nocc *.o
--- main.c
+++ main.c
@@ -1,10 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <llvm-c/Analysis.h>
+#include <llvm-c/Core.h>
 
 int main(int argc, char *argv[]) {
+    int value;
+    LLVMBuilderRef builder;
+    LLVMModuleRef module;
+    LLVMTypeRef functionType;
+    LLVMValueRef function;
+    LLVMBasicBlockRef entryBasicBlock;
+    LLVMValueRef val;
+    char *error;
+
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <src>\n", argv[0]);
         return 1;
     }
+
+    value = atoi(argv[1]);
+
+    builder = LLVMCreateBuilder();
+    module = LLVMModuleCreateWithName("main");
+    functionType = LLVMFunctionType(LLVMInt32Type(), NULL, 0, 0);
+    function = LLVMAddFunction(module, "main", functionType);
+    entryBasicBlock = LLVMAppendBasicBlock(function, "entry");
+
+    LLVMPositionBuilderAtEnd(builder, entryBasicBlock);
+    val = LLVMConstInt(LLVMInt32Type(), (unsigned long)value, 1);
+    LLVMBuildRet(builder, val);
+
+    LLVMVerifyModule(module, LLVMPrintMessageAction, &error);
+
+    if (error) {
+        fprintf(stderr, "error: %s\n", error);
+        LLVMDisposeMessage(error);
+    }
+
+    LLVMDumpModule(module);
+
+    LLVMDisposeModule(module);
+    LLVMDisposeBuilder(builder);
 
     return 0;
 }
