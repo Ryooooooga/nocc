c8b8d13318442ada132055462b429c0dc6682a4f
Ryooooooga <ryoga_314@yahoo.co.jp>
2019-04-11 22:27:53 +0900

    Fizz Buzzのサンプルコードを改善

--- examples/fizzbuzz.c
+++ examples/fizzbuzz.c
@@ -1,24 +1,5 @@
 int putchar(int c);
-
-void print_fizz(void) {
-    putchar(64 + 6);
-    putchar(96 + 9);
-    putchar(96 + 26);
-    putchar(96 + 26);
-}
-
-void print_buzz(void) {
-    putchar(64 + 2);
-    putchar(96 + 21);
-    putchar(96 + 26);
-    putchar(96 + 26);
-}
-
-void print_fizzbuzz(void) {
-    print_fizz();
-    putchar(32);
-    print_buzz();
-}
+int puts(const char *s);
 
 void print_int(int n) {
     if (n >= 10) {
@@ -32,15 +13,15 @@ void fizzbuzz(int n) {
     int i;
     for (i = 1; i <= n; i = i + 1) {
         if (i % 15 == 0) {
-            print_fizzbuzz();
+            puts("Fizz Buzz");
         } else if (i % 3 == 0) {
-            print_fizz();
+            puts("Fizz");
         } else if (i % 5 == 0) {
-            print_buzz();
+            puts("Buzz");
         } else {
             print_int(i);
+            putchar(10);
         }
-        putchar(10);
     }
 }
 
