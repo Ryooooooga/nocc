5d46318535d992eb1f62d641e5486eab6535977c
Ryooooooga <ryoga_314@yahoo.co.jp>
2019-04-13 21:33:11 +0900

    :construction: #defineディレクティブを実装

--- preprocessor.c
+++ preprocessor.c
@@ -4,6 +4,7 @@ struct Preprocessor {
     Vec *result;
     Token **tokens;
     int index;
+    Map *macros;
 };
 
 typedef struct Preprocessor Preprocessor;
@@ -68,6 +69,60 @@ void pp_concat_string(Preprocessor *pp, const Token *str) {
     t->len_string += str->len_string;
 }
 
+void pp_define(Preprocessor *pp) {
+    Token *identifier;
+    Vec *macro_tokens;
+
+    /* define */
+    if (strcmp(pp_current_token(pp)->text, "define") != 0) {
+        fprintf(stderr, "expected define, but got %s\n",
+                pp_current_token(pp)->text);
+        exit(1);
+    }
+    pp_consume_token(pp);
+
+    /* identifier */
+    identifier = pp_skip_separator(pp);
+
+    if (identifier->kind != token_identifier) {
+        fprintf(stderr, "expected identifier after #define, but got %s\n",
+                identifier->text);
+        exit(1);
+    }
+    pp_consume_token(pp);
+
+    /* macro contents */
+    macro_tokens = vec_new();
+
+    while (pp_skip_separator(pp)->kind != '\0' &&
+           pp_skip_separator(pp)->kind != '\n') {
+        vec_push(macro_tokens, pp_consume_token(pp));
+    }
+
+    /* redefinition check */
+    if (map_contains(pp->macros, identifier->text)) {
+        fprintf(stderr, "macro %s has already been defined\n",
+                identifier->text);
+        exit(1);
+    }
+
+    /* register macro */
+    map_add(pp->macros, identifier->text, macro_tokens);
+}
+
+void pp_include(Preprocessor *pp) {
+    /* include */
+    if (strcmp(pp_current_token(pp)->text, "include") != 0) {
+        fprintf(stderr, "expected include, but got %s\n",
+                pp_current_token(pp)->text);
+        exit(1);
+    }
+    pp_consume_token(pp);
+
+    fprintf(stderr, "#include not implemented\n");
+    exit(1);
+}
+
 void pp_directive(Preprocessor *pp) {
     Token *t;
 
@@ -86,8 +141,14 @@ void pp_directive(Preprocessor *pp) {
         exit(1);
     }
 
-    fprintf(stderr, "unknown preprocessor directive %s\n", t->text);
-    exit(1);
+    if (strcmp(t->text, "define") == 0) {
+        pp_define(pp);
+    } else if (strcmp(t->text, "include") == 0) {
+        pp_include(pp);
+    } else {
+        fprintf(stderr, "unknown preprocessor directive %s\n", t->text);
+        exit(1);
+    }
 }
 
 void pp_string(Preprocessor *pp) {
@@ -147,6 +208,7 @@ Vec *preprocess(const char *filename, const char *src) {
     pp.result = vec_new();
     pp.tokens = (Token **)lex(src)->data;
     pp.index = 0;
+    pp.macros = map_new();
 
     preprocess_lines(&pp);
 
