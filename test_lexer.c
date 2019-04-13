1d55071ed960e9a5653d2d797066efcba0441da6
Ryooooooga <ryoga_314@yahoo.co.jp>
2019-04-13 19:37:17 +0900

    :construction: 字句解析器がセパレータを保存するように変更

--- test_lexer.c
+++ test_lexer.c
@@ -54,36 +54,49 @@ static void test_tokens(const char *src, const TokenTestSuite *suites) {
 }
 
 void test_lexer(void) {
-    const TokenTestSuite suites1[] = {
-        {token_number, "42", 1, NULL},      {'+', "+", 1, NULL},
-        {token_number, "5", 1, NULL},       {'-', "-", 1, NULL},
-        {token_identifier, "a", 2, NULL},   {'*', "*", 2, NULL},
-        {token_identifier, "abc", 2, NULL}, {'\0', "", 2, NULL},
-    };
+    test_tokens("42  + 5 - \n a*abc", (TokenTestSuite[]){
+                                          {token_number, "42", 1, NULL},
+                                          {' ', "  ", 1, NULL},
+                                          {'+', "+", 1, NULL},
+                                          {' ', " ", 1, NULL},
+                                          {token_number, "5", 1, NULL},
+                                          {' ', " ", 1, NULL},
+                                          {'-', "-", 1, NULL},
+                                          {' ', " ", 1, NULL},
+                                          {'\n', "\n", 1, NULL},
+                                          {' ', " ", 2, NULL},
+                                          {token_identifier, "a", 2, NULL},
+                                          {'*', "*", 2, NULL},
+                                          {token_identifier, "abc", 2, NULL},
+                                          {'\0', "", 2, NULL},
+                                      });
 
-    const TokenTestSuite suites2[] = {
-        {token_if, "if", 1, NULL},
-        {'(', "(", 1, NULL},
-        {token_identifier, "x", 1, NULL},
-        {token_lesser_equal, "<=", 1, NULL},
-        {token_identifier, "y", 1, NULL},
-        {token_equal, "==", 1, NULL},
-        {token_identifier, "true", 1, NULL},
-        {')', ")", 1, NULL},
-        {'{', "{", 1, NULL},
-        {'}', "}", 2, NULL},
-        {'\0', "", 2, NULL},
-    };
+    test_tokens("if (x<=y==true) {\n}", (TokenTestSuite[]){
+                                            {token_if, "if", 1, NULL},
+                                            {' ', " ", 1, NULL},
+                                            {'(', "(", 1, NULL},
+                                            {token_identifier, "x", 1, NULL},
+                                            {token_lesser_equal, "<=", 1, NULL},
+                                            {token_identifier, "y", 1, NULL},
+                                            {token_equal, "==", 1, NULL},
+                                            {token_identifier, "true", 1, NULL},
+                                            {')', ")", 1, NULL},
+                                            {' ', " ", 1, NULL},
+                                            {'{', "{", 1, NULL},
+                                            {'\n', "\n", 1, NULL},
+                                            {'}', "}", 2, NULL},
+                                            {'\0', "", 2, NULL},
+                                        });
 
-    const TokenTestSuite suites3[] = {
-        {token_string, "\"\"", 1, ""},
-        {token_string, "\"hello\"", 1, "hello"},
-        {token_string, "\"wor\\nld\"", 2, "wor\nld"},
-        {token_string, "\"he\\\"lp\"", 2, "he\"lp"},
-        {'\0', "", 2, NULL},
-    };
-
-    test_tokens("42 + 5 - \n a*abc", suites1);
-    test_tokens("if (x<=y==true) {\n}", suites2);
-    test_tokens("\"\" \"hello\"\n\"wor\\nld\" \"he\\\"lp\"", suites3);
+    test_tokens("\"\" \"hello\"\n\"wor\\nld\" \"he\\\"lp\"",
+                (TokenTestSuite[]){
+                    {token_string, "\"\"", 1, ""},
+                    {' ', " ", 1, NULL},
+                    {token_string, "\"hello\"", 1, "hello"},
+                    {'\n', "\n", 1, NULL},
+                    {token_string, "\"wor\\nld\"", 2, "wor\nld"},
+                    {' ', " ", 2, NULL},
+                    {token_string, "\"he\\\"lp\"", 2, "he\"lp"},
+                    {'\0', "", 2, NULL},
+                });
 }
