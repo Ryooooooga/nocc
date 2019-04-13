97afe990b2c2d02e3d64642cf64c56c1229dea5d
Ryooooooga <ryoga_314@yahoo.co.jp>
2019-04-13 19:18:35 +0900

    字句解析器のテストを改善

--- test_lexer.c
+++ test_lexer.c
@@ -8,51 +8,49 @@ typedef struct {
 } TokenTestSuite;
 
 static void test_tokens(const char *src, const TokenTestSuite *suites) {
-    Vec *tokens;
-    Token *t;
-    size_t i;
+    const Token **toks = (const Token **)lex(src)->data;
+    int i = 0;
 
-    tokens = lex(src);
-
-    i = 0;
     do {
-        t = tokens->data[i];
-
-        if (t->kind != suites[i].kind) {
-            fprintf(stderr, "%s\n%d: t->kind is expected %d, but got %d\n", src,
-                    (int)i, suites[i].kind, t->kind);
+        if (toks[i]->kind != suites[i].kind) {
+            fprintf(stderr, "%s\n: toks[%d]->kind is expected %d, but got %d\n",
+                    src, i, suites[i].kind, toks[i]->kind);
             exit(1);
         }
 
-        if (strcmp(t->text, suites[i].text) != 0) {
-            fprintf(stderr, "%s\n%d: t->text is expected %s, but got %s\n", src,
-                    (int)i, suites[i].text, t->text);
+        if (strcmp(toks[i]->text, suites[i].text) != 0) {
+            fprintf(stderr, "%s\n: toks[%d]->text is expected %s, but got %s\n",
+                    src, i, suites[i].text, toks[i]->text);
+            exit(1);
+        }
+
+        if (toks[i]->line != suites[i].line) {
+            fprintf(stderr, "%s\n: toks[%d]->line is expected %d, but got %d\n",
+                    src, i, suites[i].line, toks[i]->line);
             exit(1);
         }
 
         if (suites[i].string == NULL) {
-            if (t->string != NULL) {
+            if (toks[i]->string != NULL) {
                 fprintf(stderr,
-                        "%s\n%d: t->string is expected null, but got %s\n", src,
-                        (int)i, t->string);
+                        "%s\n: toks[%d]->string is expected null, but got %s\n",
+                        src, i, toks[i]->string);
                 exit(1);
             }
         } else {
-            if (t->string == NULL) {
+            if (toks[i]->string == NULL) {
                 fprintf(stderr,
-                        "%s\n%d: t->string is expected %s, but got null\n", src,
-                        (int)i, suites[i].string);
+                        "%s\n: toks[%d]->string is expected %s, but got null\n",
+                        src, i, suites[i].string);
                 exit(1);
-            } else if (strcmp(t->string, suites[i].string) != 0) {
+            } else if (strcmp(toks[i]->string, suites[i].string) != 0) {
                 fprintf(stderr,
-                        "%s\n%d: t->string is expected %s, but got %s\n", src,
-                        (int)i, suites[i].string, t->string);
+                        "%s\n: toks[%d]->string is expected %s, but got %s\n",
+                        src, i, suites[i].string, toks[i]->string);
                 exit(1);
             }
         }
-
-        i++;
-    } while (t->kind);
+    } while (toks[i++]->kind != '\0');
 }
 
 void test_lexer(void) {
