b95c35e6afb51cba8d124c366467f98d3905da35
Ryooooooga <ryoga_314@yahoo.co.jp>
2019-04-13 10:35:13 +0900

    ソースコードをファイルから読み込むように変更

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include "nocc.h"
 
 int main(int argc, char *argv[]) {
+    const char *filename;
+    char *src;
     TranslationUnitNode *node;
     LLVMModuleRef module;
 
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <src>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
         return 1;
     }
 
-    node = parse("<stdin>", argv[1]);
+    filename = argv[1];
+    src = read_file(filename);
+
+    node = parse(filename, src);
     module = generate(node);
 
     LLVMDumpModule(module);
